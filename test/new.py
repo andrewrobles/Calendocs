667ab4e8e8b6cae7d24ccb6f45d086dd96910cc1
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2026-07-25 13:14:45 -0700

    test

--- test/new.py
+++ test/new.py
@@ -9,7 +9,6 @@ NOTES_DIR = TEST_DIR / "notes"
 CLI_PATH = TEST_DIR.parent / "app" / "bin" / "main.js"
 TEST_DATE = "2026-07-21T12:00:00"
 
-
 class TestNewNote(unittest.TestCase):
     def setUp(self):
         shutil.rmtree(NOTES_DIR, ignore_errors=True)
@@ -21,7 +20,7 @@ class TestNewNote(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(NOTES_DIR, ignore_errors=True)
 
-    def test_new_note(self):
+    def test_new_test_note(self):
         '''
         $ note new "test"
         Created note: "test"
@@ -64,6 +63,34 @@ README.md
         '''
         stdout = '[21](./README.md)\n\n* [test](./21/test.md)'
         result = subprocess.run(["cat", str(NOTES_DIR / "21.md")], capture_output=True, text=True, check=True)
+        self.assertEqual(result.stdout, stdout)
+
+    def test_new_day_note(self):
+        '''
+        $ note new "test"
+        Created note: "test"
+        '''
+        env = os.environ.copy()
+        env["NOTES_TEST_DATE"] = TEST_DATE
+        result = subprocess.run(["node", str(CLI_PATH), "new"], cwd=NOTES_DIR, env=env, check=True, capture_output=True, text=True)
+        expected = 'Created note: "21"'
+        self.assertEqual(result.stdout, expected)
+
+        '''
+        $ cat README.md
+        '''
+        stdout = '''July
+
+| S | M | T | W | T | F | S |
+|---|---|---|---|---|---|---|
+|   |   |   | 1 | 2 | 3 | 4 |
+| 5 | 6 | 7 | 8 | 9 | 10 | 11 |
+| 12 | 13 | 14 | 15 | 16 | 17 | 18 |
+| 19 | 20 | [21](./21.md) | 22 | 23 | 24 | 25 |
+| 26 | 27 | 28 | 29 | 30 | 31 |   |
+
+'''
+        result = subprocess.run(["cat", str(NOTES_DIR / "README.md")], capture_output=True, text=True, check=True)
         self.assertEqual(result.stdout, stdout)
 
 
